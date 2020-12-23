575c129c32ce9839e9ff3afd4b570bc025773061
Traveler2019 <47352395+Traveler2019@users.noreply.github.com>
2020-12-23 16:49:55 +0530

    Removal of unwanted Plugins
    Removed cloudinary and uploadcare setups

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'No Code Tutorials',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Learn how to use the popular no code technologies with solutions to the most common problems',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
