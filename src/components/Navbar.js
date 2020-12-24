6a8ff537b24ef947e3ce4ff4256a6da0fa63d591
Traveler2019 <47352395+Traveler2019@users.noreply.github.com>
2020-12-24 18:04:55 +0530

    added a no code logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import siteLogo from '../img/no-code-text-logo.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={siteLogo} alt="No Code text logo" style={{ width: '200px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -60,13 +60,13 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
+              <Link className="navbar-item has-text-weight-semibold" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/blog">
+              <Link className="navbar-item has-text-weight-semibold" to="/blog">
                 Blog
               </Link>
-              <Link className="navbar-item" to="/contact">
+              <Link className="navbar-item has-text-weight-semibold" to="/contact">
                 Contact
               </Link>
             </div>
